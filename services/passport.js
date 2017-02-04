103013cc0c63ada49c7f6e8cfbfe10823dd970b7
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-02-03 16:49:31 -0800

    Config

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const User = require('../models/user');
-require('../config/config');
+const config = require('../config/config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const LocalStrategy = require('passport-local');
